933db75fa225c7e782156b4c02719c296349f83e
Philipp Schirmer <philipp.schirmer@bakdata.com>
2021-09-02 14:56:33 +0200

    Upload snapshot from branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     `java-library`
     id("net.researchgate.release") version "2.8.1"
     id("org.sonarqube") version "3.3"
+    id("io.codearte.nexus-staging") version "0.30.0"
     id("com.bakdata.sonar") version "1.1.6"
     id("com.bakdata.sonatype") version "1.1.6"
     id("org.hildan.github.changelog") version "1.7.0"
