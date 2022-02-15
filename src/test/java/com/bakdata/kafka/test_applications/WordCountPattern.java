ad3be4a8277d57e0e4e89686cae550a630b3f6df
Philipp Schirmer <philipp.schirmer@bakdata.com>
2022-02-15 11:09:05 +0100

    Fix clean for pattern subscriptions

--- src/test/java/com/bakdata/kafka/test_applications/WordCountPattern.java
+++ src/test/java/com/bakdata/kafka/test_applications/WordCountPattern.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 bakdata
+ * Copyright (c) 2022 bakdata
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -62,7 +62,7 @@ public class WordCountPattern extends KafkaStreamsApplication {
 
     @Override
     public String getUniqueAppId() {
-        return this.getClass().getSimpleName() + "-" + this.getInputTopic() + "-" + this.getOutputTopic();
+        return this.getClass().getSimpleName() + "-" + this.getOutputTopic();
     }
 
     @Override
