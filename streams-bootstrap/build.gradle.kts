204db8843bcff7dc9778f50d192344838d605c48
Philipp Schirmer <philipp.schirmer@bakdata.com>
2024-01-09 18:00:12 +0100

    Update dependencies (#196)

--- streams-bootstrap/build.gradle.kts
+++ streams-bootstrap/build.gradle.kts
@@ -8,30 +8,30 @@ dependencies {
     val kafkaVersion: String by project
     implementation(group = "org.apache.kafka", name = "kafka-tools", version = kafkaVersion)
 
-    implementation(group = "info.picocli", name = "picocli", version = "4.7.0")
+    implementation(group = "info.picocli", name = "picocli", version = "4.7.5")
     api(group = "org.apache.kafka", name = "kafka-streams", version = kafkaVersion)
     api(group = "org.apache.kafka", name = "kafka-clients", version = kafkaVersion)
     val confluentVersion: String by project
     implementation(group = "io.confluent", name = "kafka-streams-avro-serde", version = confluentVersion)
     api(group = "io.confluent", name = "kafka-schema-registry-client", version = confluentVersion)
-    val log4jVersion = "2.21.1"
+    val log4jVersion = "2.22.1"
     implementation(group = "org.apache.logging.log4j", name = "log4j-core", version = log4jVersion)
     implementation(group = "org.apache.logging.log4j", name = "log4j-slf4j2-impl", version = log4jVersion)
     api(
         group = "org.slf4j",
         name = "slf4j-api",
-        version = "2.0.6"
+        version = "2.0.9"
     ) // required because other dependencies use Slf4j 1.x which is not properly resolved if this library is used in test scope
-    implementation(group = "com.google.guava", name = "guava", version = "31.1-jre")
+    implementation(group = "com.google.guava", name = "guava", version = "33.0.0-jre")
     implementation(group = "org.jooq", name = "jool", version = "0.9.14")
 
-    val junitVersion = "5.9.1"
+    val junitVersion = "5.10.1"
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = junitVersion)
-    testImplementation(group = "org.junit-pioneer", name = "junit-pioneer", version = "1.9.1")
+    testImplementation(group = "org.junit-pioneer", name = "junit-pioneer", version = "2.2.0")
     testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
-    testImplementation(group = "org.assertj", name = "assertj-core", version = "3.23.1")
-    val mockitoVersion = "4.11.0"
+    testImplementation(group = "org.assertj", name = "assertj-core", version = "3.25.1")
+    val mockitoVersion = "5.8.0"
     testImplementation(group = "org.mockito", name = "mockito-core", version = mockitoVersion)
     testImplementation(group = "org.mockito", name = "mockito-junit-jupiter", version = mockitoVersion)
 
