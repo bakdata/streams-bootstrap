d6a53ae19cdc429bd1744143652cc848a52699c5
Philipp Schirmer <philipp.schirmer@bakdata.com>
2024-01-30 13:27:05 +0100

    Fix Jacoco for Java 17 (#201)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("net.researchgate.release") version "3.0.2"
-    id("com.bakdata.sonar") version "1.1.9"
-    id("com.bakdata.sonatype") version "1.1.9"
+    id("com.bakdata.sonar") version "1.1.11"
+    id("com.bakdata.sonatype") version "1.1.11"
     id("org.hildan.github.changelog") version "1.12.1"
-    id("io.freefair.lombok") version "6.6.1"
+    id("io.freefair.lombok") version "6.6.3"
 }
 
 allprojects {
