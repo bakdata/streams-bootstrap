69a8c95001790adc1862d608c18c567c94aab2bc
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-01-06 17:34:53 +0100

    Fix tests

--- streams-bootstrap-core/src/test/java/com/bakdata/kafka/test_applications/ComplexTopologyApplication.java
+++ streams-bootstrap-core/src/test/java/com/bakdata/kafka/test_applications/ComplexTopologyApplication.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2024 bakdata
+ * Copyright (c) 2025 bakdata
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -66,7 +66,7 @@ public class ComplexTopologyApplication implements StreamsApp {
 
     @Override
     public String getUniqueAppId(final StreamsTopicConfig topics) {
-        return this.getClass().getSimpleName() + "-" + topics.getOutputTopic();
+        return ComplexTopologyApplication.class.getSimpleName() + "-" + topics.getOutputTopic();
     }
 
     @Override
