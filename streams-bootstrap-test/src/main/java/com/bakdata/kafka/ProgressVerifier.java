dbe16f74218985ecfe67d162ff383707e2952220
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-01-17 19:11:48 +0100

    Update

--- streams-bootstrap-test/src/main/java/com/bakdata/kafka/ProgressVerifier.java
+++ streams-bootstrap-test/src/main/java/com/bakdata/kafka/ProgressVerifier.java
@@ -31,8 +31,10 @@ import com.bakdata.util.seq2.PairSeq;
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.ConsumerGroupDescription;
 import org.apache.kafka.clients.admin.ListOffsetsResult.ListOffsetsResultInfo;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
@@ -42,56 +44,96 @@ import org.apache.kafka.common.ConsumerGroupState;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 
+/**
+ * Utility class to verify the state of Kafka consumer groups and topics
+ */
+@Slf4j
 @RequiredArgsConstructor
 public class ProgressVerifier {
 
     private final KafkaTestClient testClient;
 
+    /**
+     * Check whether consumer group has state {@link ConsumerGroupState#STABLE}
+     * @param group consumer group name
+     * @return true if consumer group has state {@link ConsumerGroupState#STABLE}
+     */
     public boolean isActive(final String group) {
         return this.getState(group) == ConsumerGroupState.STABLE;
     }
 
+    /**
+     * Check whether consumer group has state {@link ConsumerGroupState#EMPTY}
+     * @param group consumer group name
+     * @return true if consumer group has state {@link ConsumerGroupState#EMPTY}
+     */
     public boolean isClosed(final String group) {
         return this.getState(group) == ConsumerGroupState.EMPTY;
     }
 
+    /**
+     * Get current state of consumer group
+     * @param group consumer group name
+     * @return current state of consumer group
+     */
+    public ConsumerGroupState getState(final String group) {
+        try (final ImprovedAdminClient admin = this.testClient.admin();
+                final ConsumerGroupClient consumerGroupClient = admin.getConsumerGroupClient()) {
+            final ConsumerGroupDescription description = consumerGroupClient.describe(group);
+            final ConsumerGroupState state = description.state();
+            log.debug("Consumer group '{}' has state {}", group, state);
+            return state;
+        }
+    }
+
+    /**
+     * Check whether consumer group has assigned partitions and lag is 0
+     * @param group consumer group name
+     * @return true if consumer group has assigned partitions and lag is 0
+     */
     public boolean hasFinishedProcessing(final String group) {
-        return this.computeLag(group) == 0;
+        return this.computeLag(group).filter(lag -> lag == 0).isPresent();
     }
 
-    public boolean hasMessages(final String topic, final int numberOfMessages, final Duration timeout) {
-        final List<ConsumerRecord<byte[], byte[]>> records = this.testClient.read()
-                .with(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class)
-                .with(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class)
-                .from(topic, timeout);
-        return records.size() == numberOfMessages;
-    }
-
-    private long computeLag(final String group) {
+    /**
+     * Compute lag of consumer group
+     * @param group consumer group name
+     * @return lag of consumer group. If no partitions are assigned, an empty {@code Optional} is returned
+     */
+    public Optional<Long> computeLag(final String group) {
         try (final ImprovedAdminClient admin = this.testClient.admin();
                 final ConsumerGroupClient consumerGroupClient = admin.getConsumerGroupClient();
                 final TopicClient topicClient = admin.getTopicClient()) {
             final Map<TopicPartition, OffsetAndMetadata> consumerOffsetsMap = consumerGroupClient.listOffsets(group);
+            log.debug("Consumer group '{}' has {} subscribed partitions", group, consumerOffsetsMap.size());
             final PairSeq<TopicPartition, Long> consumerOffsets = PairSeq.seq(consumerOffsetsMap)
                     .mapValues(OffsetAndMetadata::offset);
             final Map<TopicPartition, ListOffsetsResultInfo> partitionOffsetsMap =
                     topicClient.listOffsets(consumerOffsetsMap.keySet());
             final PairSeq<TopicPartition, Long> partitionOffsets = PairSeq.seq(partitionOffsetsMap)
                     .mapValues(ListOffsetsResultInfo::offset);
-            return consumerOffsets.innerJoinByKey(partitionOffsets)
+            final Optional<Long> lag = consumerOffsets.innerJoinByKey(partitionOffsets)
                     .values()
                     .mapToPair(Function.identity())
                     .map((consumerOffset, latestOffset) -> latestOffset - consumerOffset)
-                    .sum()
-                    .orElse(0L);
+                    .sum();
+            log.debug("Consumer group '{}' has lag {}", group, lag);
+            return lag;
         }
     }
 
-    private ConsumerGroupState getState(final String group) {
-        try (final ImprovedAdminClient admin = this.testClient.admin();
-                final ConsumerGroupClient consumerGroupClient = admin.getConsumerGroupClient()) {
-            final ConsumerGroupDescription description = consumerGroupClient.describe(group);
-            return description.state();
-        }
+    /**
+     * Read all messages from a topic as bytes
+     * @param topic topic name
+     * @param timeout consumer poll timeout
+     * @return all messages from topic
+     */
+    public List<ConsumerRecord<byte[], byte[]>> readAllMessages(final String topic, final Duration timeout) {
+        final List<ConsumerRecord<byte[], byte[]>> records = this.testClient.read()
+                .with(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class)
+                .with(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class)
+                .from(topic, timeout);
+        log.debug("Topic '{}' has {} messages", records, records.size());
+        return records;
     }
 }
