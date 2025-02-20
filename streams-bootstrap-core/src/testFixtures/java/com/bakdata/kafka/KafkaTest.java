d787fdb4a1cd036f0e36c11196f7c448e8e216c6
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-02-20 09:57:49 +0100

    Update

--- streams-bootstrap-core/src/testFixtures/java/com/bakdata/kafka/KafkaTest.java
+++ streams-bootstrap-core/src/testFixtures/java/com/bakdata/kafka/KafkaTest.java
@@ -44,7 +44,7 @@ public abstract class KafkaTest {
         return new KafkaContainer(DockerImageName.parse("apache/kafka-native")
                 .withTag("3.9.0"))
                 .withEnv("KAFKA_LISTENERS",
-                        "PLAINTEXT://:9092,BROKER://:9093,CONTROLLER://:9094"); //TODO remove with 3.9.1
+                        "PLAINTEXT://:9092,BROKER://:9093,CONTROLLER://:9094"); //TODO remove with 3.9.1 https://issues.apache.org/jira/browse/KAFKA-18281
     }
 
     private static ConditionFactory await() {
