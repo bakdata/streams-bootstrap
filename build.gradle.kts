04a3bc79d423f582c46353fc724af64e14a7ca51
Torben Meyer <torben.meyer@bakdata.com>
2025-04-08 09:35:10 +0200

    Simplify

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,16 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import nl.littlerobots.vcu.plugin.resolver.VersionSelectors
 
 plugins {
     alias(libs.plugins.release)
     alias(libs.plugins.sonar)
     alias(libs.plugins.sonatype)
     alias(libs.plugins.lombok)
-    id("com.github.ben-manes.versions") version "0.52.0"
     id("nl.littlerobots.version-catalog-update") version "1.0.0"
-}
-
-tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
-    rejectVersionIf {
-        isNonStable(candidate.version)
-    }
 }
 
 versionCatalogUpdate {
     sortByKey.set(false)
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
+    versionSelector(VersionSelectors.STABLE)
 }
 
 allprojects {
