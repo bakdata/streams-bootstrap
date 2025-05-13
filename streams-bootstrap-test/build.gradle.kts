bb9f4c490a06fce1fc4114450d34b06fc2494358
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-05-13 18:01:42 +0200

    Add tests

--- streams-bootstrap-test/build.gradle.kts
+++ streams-bootstrap-test/build.gradle.kts
@@ -2,6 +2,7 @@ description = "Utils for testing your Kafka Streams Application"
 
 plugins {
     id("java-library")
+    alias(libs.plugins.avro)
 }
 
 dependencies {
@@ -12,6 +13,9 @@ dependencies {
     testRuntimeOnly(libs.junit.platform.launcher)
     testImplementation(libs.junit.jupiter)
     testImplementation(libs.assertj)
+    testImplementation(libs.kafka.streams.avro.serde) {
+        exclude(group = "org.apache.kafka", module = "kafka-clients") // force usage of OSS kafka-clients
+    }
     testImplementation(libs.kafka.protobuf.provider) {
         exclude(group = "org.apache.kafka", module = "kafka-clients") // force usage of OSS kafka-clients
     }
