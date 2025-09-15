acc6c2cd0567cebc5e6ee27c29a83db4cb9a2583
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-09-15 15:54:10 +0200

    Clean up

--- streams-bootstrap-core/src/main/java/com/bakdata/kafka/streams/kstream/KTableXImpl.java
+++ streams-bootstrap-core/src/main/java/com/bakdata/kafka/streams/kstream/KTableXImpl.java
@@ -624,6 +624,16 @@ class KTableXImpl<K, V> implements KTableX<K, V> {
     }
 
     @Override
+    public KTableX<K, V> withLineage(final MaterializedX<K, V, KeyValueStore<Bytes, byte[]>> materialized) {
+        return this.transformValues(LineageTransformer::new, materialized);
+    }
+
+    @Override
+    public KTableX<K, V> withLineage(final MaterializedX<K, V, KeyValueStore<Bytes, byte[]>> materialized, final Named named) {
+        return this.transformValues(LineageTransformer::new, materialized, named);
+    }
+
+    @Override
     public String queryableStoreName() {
         return this.wrapped.queryableStoreName();
     }
