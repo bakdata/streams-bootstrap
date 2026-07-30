ec6152a8375155d4c0874b6051fcfb8296aa8286
Philipp Schirmer <philipp.schirmer@bakdata.com>
2026-07-30 19:32:46 +0200

    Upgrade to fluent-kafka 4.0

--- streams-bootstrap-core/src/test/java/com/bakdata/kafka/streams/AvroMirrorTest.java
+++ streams-bootstrap-core/src/test/java/com/bakdata/kafka/streams/AvroMirrorTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2025 bakdata
+ * Copyright (c) 2026 bakdata
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,7 +24,7 @@
 
 package com.bakdata.kafka.streams;
 
-import com.bakdata.fluent_kafka_streams_tests.junit5.TestTopologyExtension;
+import com.bakdata.fluent_kafka_streams_tests.junitjupiter.TestTopologyExtension;
 import com.bakdata.kafka.TestRecord;
 import com.bakdata.kafka.streams.apps.MirrorWithNonDefaultSerde;
 import java.util.List;
